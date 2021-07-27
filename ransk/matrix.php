ab106538cd579c331fb41f2db744ab6ae90049e6
anmolk11 <79657826+anmolk11@users.noreply.github.com>
2021-07-27 22:36:14 +0530

    Add files via upload

---
+++ ransk/matrix.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+?>
+
+<?php
+include '../conn.php';
+$query = "SELECT user_info.fname, user_info.lname,data.Matrix FROM user_info LEFT JOIN data ON user_info.reg=data.reg ORDER by data.Matrix DESC";
+$res = mysqli_query($conn, $query);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '../links.php'?>
+    <title>Table</title>
+    <link rel="stylesheet" href="table.css">
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Score</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $rank = 1;
+            while ($ans = mysqli_fetch_array($res)) {
+            ?>
+                <tr>
+                    <td><?php echo $rank ?></td>
+                    <td><?php echo $ans['fname'] . ' ' . $ans['lname']; ?></td>
+                    <td><?php echo $ans['Matrix']; ?></td>
+                </tr>
+            <?php
+                $rank++;
+            } ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
